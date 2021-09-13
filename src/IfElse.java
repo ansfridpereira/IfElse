3cafc09ea2f85092bf5e1ded66398f4a0cc21124
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-09-13 18:33:42 +0530

    If Else statement Champ if full marks, just passed if 8 marks and normal if marks betwen 8 to 20

---
+++ src/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args)
+    {
+        int marks=10;
+
+        if(marks==20)
+            System.out.println("You are Champ");
+        if(marks==8)
+            System.out.println("You Just Managed to Pass");
+        else
+            System.out.println("You are a normal person");
+
+    }
+}
